50a9f0206a0a24374c61ae536d42b13a8b93172a
leofuna <leo.funa@gmail.com>
2023-11-30 20:12:11 -0300

    feat: loading layer

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import LoadingSpinner from "@/components/LoadingSpinner";
+
+export default function Loading() {
+  return (
+    <div className="flex items-center p-10 justify-center">
+      <LoadingSpinner />
+    </div>
+  )
+}
